5b96818ad4c19b455102b00d5fbe33acbc69209a
Andres Carranza <diacarry@gmail.com>
2019-09-26 07:02:07 -0500

    Adicion de atributos adicionales del modelo USER y cambio de permisos de administrador a los ficheros del aplicativo

--- app/User.php
+++ app/User.php
@@ -6,19 +6,53 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use Notifiable;
-
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'email';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * Options: true -> Auto-Increment; false -> No Auto-Increment
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+    /**
+     * Indicates if the model should be timestamped.
+     * Options: true -> able; false -> enable
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * Names variables CREATED_AT and UPDATED_AT (default)
+     */
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'perfil', 'nickname', 'city',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -27,7 +61,6 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
